0e5fbc015d9a9f3ec50576f030836fa8de5cd756
cung nguyen <cung.fpt@carehealth.io>
2024-09-30 20:33:19 +0700

    day-09-first-commit

---
+++ day-09/function.py
@@ -0,0 +1,16 @@
+#syntax
+def functionName(parameter1, parameter2):
+    #content
+    return parameter1
+
+def total(parameter1, parameter2):
+    print(f"param1: ", parameter1, "param2: ", parameter2)
+    a = str(parameter1) + parameter2
+    return a
+
+#call function
+totalParameter = total(1, "nhung") #1,2 doi so
+# print(totalParameter) 
+
+
+
